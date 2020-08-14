9d11248fda5c34a4db9eca62b8a8c60ac1da2496
vikascowdary47 <vikaschowdary47@gmail.com>
2020-08-14 23:16:03 +0530

    handle change

--- todo/src/components/Add.js
+++ todo/src/components/Add.js
@@ -2,21 +2,31 @@ import React from "react";
 import { TextField, Button } from "@material-ui/core";
 
 const Add = () => {
+  const [add, setAdd] = React.useState("");
+  const [addList, setAddList] = React.useState("");
   return (
     <div
       style={{
         marginTop: "30px",
       }}
     >
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setAddList(add);
+          console.log(addList);
+        }}
+      >
         <TextField
           size="small"
           id="outlined-basic"
           label="Add details"
           variant="outlined"
+          value={add}
+          onChange={(e) => setAdd(e.target.value)}
           style={{ width: "60%", marginRight: "3px" }}
         />
-        <Button size="medium" variant="contained" color="primary">
+        <Button size="medium" variant="contained" color="primary" type="submit">
           Add
         </Button>
       </form>
--- todo/src/components/List.js
+++ todo/src/components/List.js
@@ -2,17 +2,26 @@ import React from "react";
 import { FormControlLabel, Checkbox } from "@material-ui/core";
 
 const List = () => {
+  const [check, setCheck] = React.useState(false);
   return (
     <div>
       <FormControlLabel
         control={
           <Checkbox
-          // checked={state.checkedA}
-          // onChange={handleChange}
-          // name="checkedA"
+            // checked={state.checkedA}
+            onChange={(e) => setCheck(true)}
+            // name="checkedA"
           />
         }
-        label="can this be a list item"
+        label={
+          <p>
+            {check ? (
+              <del>can a list be here in label</del>
+            ) : (
+              <p> can a list be here in label</p>
+            )}
+          </p>
+        }
       />
     </div>
   );
